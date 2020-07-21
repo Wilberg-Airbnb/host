9dbab5bb46ec8b9ef7e680d6b206c7f2d3e11162
Krissy Do <krissytdo@hotmail.com>
2020-07-21 00:10:12 -0700

    complete header for host service

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -4,15 +4,16 @@ import { Image } from 'semantic-ui-react';
 const badge = `${window.location.href}superhost_badge.png`;
 
 const badgePosition = {
-  position: 'relative',
-  left: '45px',
-  bottom: '23px'
+  position: 'absolute',
+  left: '47px',
+  top: '35px'
 };
 
 const Header = ({name, superhost, img, joined}) => (
-  <div style={{border: '1px solid black'}}>
-  <Image circular width={56} src={img}/>
-  {superhost === true ? <Image height={23} position={'absolute'} right={'0px'} bottom={'0px'} src={badge} style={badgePosition}/> : null}
+  <div style={{ display: 'flex', marginBottom: '24px'}}>
+  <Image style={{marginRight: '16px'}} circular width={56} src={img}/>
+  {superhost ? <Image height={23} src={badge} style={badgePosition} /> : null}
+  {name !== null ? <div><h2 style={{marginBottom: '0px'}}>Hosted by {name.split(' ')[0]}</h2><p style={{color: 'rgb(113, 113, 113)'}}>Joined in {joined.split(' ')[1]} {joined.split(' ')[3]} </p> </div>: null }
   </div>
 )
 
