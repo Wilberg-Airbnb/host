380d715e30efd579aa921d9d2635bb97db7f342f
Krissy Do <krissytdo@hotmail.com>
2020-07-24 15:34:37 -0700

    added babel plugin for material ui

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Image } from 'semantic-ui-react';
 
-const badge = `${window.location.href}superhost_badge.png`;
+const badge = `http://3.12.169.208:2000/public/superhost_badge.png`;
 
 const badgePosition = {
   position: 'absolute',
@@ -10,7 +10,7 @@ const badgePosition = {
 };
 
 const Header = ({name, superhost, img, joined}) => (
-  <div style={{ display: 'flex', marginBottom: '24px'}}>
+  <div style={{ display: 'flex', marginBottom: '24px', position: 'relative'}}>
   <Image style={{marginRight: '16px'}} circular width={56} src={img}/>
   {superhost ? <Image height={23} src={badge} style={badgePosition} /> : null}
   {name !== null ? <div><h2 style={{marginBottom: '0px'}}>Hosted by {name.split(' ')[0]}</h2><p style={{color: 'rgb(113, 113, 113)'}}>Joined in {joined.split(' ')[1]} {joined.split(' ')[3]} </p> </div>: null }
